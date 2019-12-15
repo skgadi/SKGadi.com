85c6d4896870b975be83c7430eb0fff3b046c972
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2019-12-14 20:33:17 -0600

    Automatic push

--- public/js/script003.js
+++ public/js/script003.js
@@ -1,5 +1,5 @@
 function Print() {
-	mywindow = PrintElem('PrintContent', ['https://cdn.jsdelivr.net/gh/skgadi/cm-wf/fonts.min.css', '/css/resume-print.css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'], []);
+	mywindow = PrintElem('PrintContent', ['https://cdn.jsdelivr.net/gh/skgadi/wf@1.0/fonts/u57c.min.css', '/css/resume-print.css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css'], []);
 	$(mywindow).bind('load', function(){
 		setTimeout(function(){
 			mywindow.focus();
