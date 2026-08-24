828fe76fce5bffdc217a7d40d228b84a4b590371
Suresh Kumar Gadi <skgadi@users.noreply.github.com>
2026-08-24 15:36:25 -0600

    Automatic push

---
+++ public/js/script003.js
@@ -0,0 +1,20 @@
+function Print() {
+	mywindow = PrintElem('PrintContent', ['https://cdn.jsdelivr.net/gh/skgadi/wf@1.0/fonts/u57c.min.css', '/css/resume-print.css', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css'], []);
+	$(mywindow).bind('load', function(){
+		setTimeout(function(){
+			mywindow.focus();
+			mywindow.print();
+			//mywindow.close();
+		}, 1000);
+
+	})
+	
+}
+
+$(document).bind('keydown', function(e) {
+	var code = (e.keyCode ? e.keyCode : e.which);
+	if (code == 80) {
+		Print();
+		return false;
+	}            
+});
